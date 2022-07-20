d4ca060bfee59195c5041281c6ca075760f21dff
eric-klimowich <eklimowich@hotmail.com>
2022-07-20 08:41:44 -0400

    Initial commit

---
+++ main_app/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Cat, Toy
+from .forms import FeedingForm
+
+# View functions
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def cats_index(request):
+  cats = Cat.objects.all()
+  return render(request, 'cats/index.html', { 'cats': cats })
+
+def cats_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  # instantiate FeedingForm to be rendered in the detail.html template
+  feeding_form = FeedingForm()
+  return render(request, 'cats/detail.html', {
+    'cat': cat,
+    'feeding_form': feeding_form
+  })
+
+class CatCreate(CreateView):
+  model = Cat
+  fields = '__all__'
+
+class CatUpdate(UpdateView):
+  model = Cat
+  fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats/'
+
+def add_feeding(request, cat_id):
+  # create a ModelForm instance using the data in the posted form
+  form = FeedingForm(request.POST)
+  # validate the data
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('detail', cat_id=cat_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
